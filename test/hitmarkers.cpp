719e87bff82d1d58bfc18c87f88b24b56601b975
Brent Bessemer <bmb0610@gmail.com>
2017-12-13 10:01:59 -0500

    Added test (hitmarkers)

---
+++ test/hitmarkers.cpp
@@ -0,0 +1,36 @@
+/*
+ * hitmarkers v3.2 (MLG Simulator 2006, Static Edition)
+ *
+ * Code copyright (C) 2017 Brent Bessemer.
+ * All rights reserved.
+ *
+ * Original concept (as "MLG Simulator 2004" and "MLG Simulator 2005")
+ * and assets copyright (C) 2014-2017 Seth Carter, if it's even possible to
+ * copyright the concept of spraying a bunch of gaming-related logos and memes
+ * onto a screen with 69 pixel-art hitmarkers and a black-and-white GIF of
+ * Snoop Dogg dancing while shitty dubstep (is there any other kind?) plays
+ * in the background.
+ *
+ * DISCLAIMER: ALL COPYRIGHTED IMAGES, MUSIC, SOUNDS, TRADEMARKS, LIKENESSES,
+ * OR OTHER FORMS OF INTELLECTUAL PROPERTY DISPLAYED OR FEATURED HEREIN ARE
+ * STRICTLY THE PROPERTY OF THEIR RESPECTIVE OWNERS. AUTHOR HEREBY CLAIMS FAIR
+ * USE OF ANY COPYRIGHTED AND/OR TRADEMARKED ASSETS FEATURED HEREIN FOR THE
+ * PURPOSE OF PARODY, AND OTHER STRICTLY NON-COMMERCIAL PURPOSES.
+ */
+
+#include <phoenix/pse.h>
+
+// MLG #defines
+#define NOSCOPE 360.0
+#define reload() srand(SDL_GetPerformanceCounter())
+
+namespace mlg {
+
+// Textures
+px::Texture
+four[20],   // Snoop Dogg GIF (20 frames)
+hitmarker_tex,
+meme_texes[13],
+fashion_texes[44];
+
+}
