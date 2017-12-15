6d85b78b00d93b655fd6b1a5fd9ff234ea2b7ed4
Brent Bessemer <bmb0610@gmail.com>
2017-12-15 11:02:50 -0500

    Master header

---
+++ include/phoenix/pse.h
@@ -0,0 +1,13 @@
+/*
+ * Copyright (C) 2017 Brent Bessemer.
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include "box.h"
+#include "color.h"
+#include "container.h"
+#include "entity.h"
+#include "geometry.h"
+#include "renderer.h"
