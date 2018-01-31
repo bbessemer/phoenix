7cb3ea9f5688ff93e5c5f59d3da8e8fa035717ac
Brent Bessemer <bmb0610@gmail.com>
2018-01-31 13:34:42 -0500

    Textures (hardcoded)

--- test/dots.c
+++ test/dots.c
@@ -12,6 +12,32 @@
 #include <time.h>
 #include <phoenix/phoenix.h>
 
+static px_color_t wamba_tex[8][8];
+static px_color_t food_tex[6][6];
+static px_color_t poison_tex[6][6];
+
+void MakeTextures () {
+    // Wamba
+    for (int i = 0; i < 64; i++)
+        pxSetColor_RGBAi(wamba_tex + i, 11, 123, 255, 255); // Background
+    for (int i = 1; i <= 3; i++) {
+        wamba_tex[i][2] = (px_color_t){0, 0, 0, 1}; // Left eye
+        wamba_tex[i][5] = (px_color_t){0, 0, 0, 1}; // Right eye
+    }
+
+    // Food
+    for (int i = 0; i < 36; i++)
+        pxSetColor_RGBAi(food_tex + i, 235, 61, 0, 255);
+    food_tex[1][1] = (px_color_t){0, 0, 0, 1};
+    food_tex[1][4] = (px_color_t){0, 0, 0, 1};
+
+    // Poison
+    for (int i = 0; i < 36; i++)
+        pxSetColor_RGBAi(poison_tex + i, 0, 249, 0, 255);
+    poison_tex[1][1] = (px_color_t){0, 0, 0, 1};
+    poison_tex[1][4] = (px_color_t){0, 0, 0, 1};
+}
+
 #define ROUND_TIME 1.6
 static px_box_t character, food, poison;
 static float ttl;
