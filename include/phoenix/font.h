dd01d06a4601d826af847067f00f3c0a4aeee5ba
Brent Bessemer <bmb0610@gmail.com>
2018-02-01 12:43:16 -0500

    Bitmap fonts

---
+++ include/phoenix/font.h
@@ -0,0 +1,20 @@
+/*
+ * Copyright (C) 2017-2018 Brent Bessemer.
+ * All rights reserved.
+ */
+
+#pragma once
+
+#include <phoenix/texture.h>
+
+typedef struct px_bmpfont {
+    unsigned int char_w, char_h;
+    char chars[256];
+    // Data follows.
+} px_bmpfont_t;
+
+void pxMakeBitmapFont (px_bmpfont_t *font, px_image_t *image,
+    const char *string, int char_h);
+void pxMakeDefaultFont (px_bmpfont_t *font);
+void pxDrawBitmapChar (px_image_t *out, const px_bmpfont_t *font,
+    const size_t i, char c);
---
+++ src/bmpfont.c
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2017-2018 Brent Bessemer.
+ * All rights reserved.
+ */
+
+#include <string.h>
+#include <phoenix/font.h>
+
+void pxMakeBitmapFont (px_bmpfont_t *font, px_image_t *image,
+    const char *string, int char_h)
+{
+    font->char_w = image->w;
+    font->char_h = char_h;
+    size_t len = strlen(string) + 1;
+    strncpy(font->chars, string, 256);
+    if (len > 256) {
+        font->chars[255] = 0;
+        len = 256;
+    }
+    memcpy(font + 1, image + 1, char_h * image->w * len);
+}
+
+extern struct {} px_dflt_font_img;
+
+void pxMakeDefaultFont (px_bmpfont_t *font) {
+    pxMakeBitmapFont(font, (px_image_t *) &px_dflt_font_img,
+        "ABCDEFGHIJKLMNOPQRSTUVWZYZabcdefghijklmnopqrstuvwzyz0123456789-", 5);
+}
+
+void pxDrawBitmapChar (px_image_t *out, const px_bmpfont_t *font,
+    const size_t i, char c)
+{
+    px_icolor_t *img_data = (px_icolor_t *)(out + 1);
+    px_icolor_t *font_data = (px_icolor_t *)(font + 1);
+    int char_i = 0;
+    for (int j = 0; 1; j++) if (font->chars[j] == c || !font->chars[j]) {
+        char_i = j;
+        break;
+    }
+    for (int j = 0; j < font->char_h; j++) {
+        memcpy(img_data + (j * out->w) + (i * font->char_w),
+            font_data + (char_i * font->char_h * font->char_w)
+            + (j * font->char_w), font->char_w * sizeof(px_icolor_t));
+    }
+}
