c34b73da5a3f36163a6f9be80e726c996f754b85
Brent Bessemer <bmb0610@gmail.com>
2018-02-02 11:36:27 -0500

    Defines for texture functions

--- include/phoenix/texture.h
+++ include/phoenix/texture.h
@@ -13,3 +13,11 @@ px_tex_t pxMakeTexture_gl (px_image_t *img);
 px_tex_t pxMakeTexture_Raw_gl (unsigned int w, unsigned int h,
     px_icolor_t *pixels);
 px_tex_t pxRemakeTexture_gl (px_tex_t old, px_image_t *new_img);
+
+#ifdef PX_USE_GL
+
+#define pxMakeTexture       pxMakeTexture_gl
+#define pxMakeTexture_Raw   pxMakeTexture_Raw_gl
+#define pxRemakeTexture     pxRemakeTexture_gl
+
+#endif
