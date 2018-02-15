6ee1bd2fb3aa0a9ee0567983a649b303c9f0e636
Brent Bessemer <bmb0610@gmail.com>
2018-02-15 11:15:56 -0500

    A little bit of Rust audio

--- pxaudio/src/lib.rs
+++ pxaudio/src/lib.rs
@@ -3,10 +3,22 @@
  * All rights reserved.
  */
 
+extern crate sdl2_sys;
 use std::slice;
+use std::mem;
+use sdl2_sys::{
+    SDL_AudioSpec,
+    SDL_OpenAudio,
+    SDL_InitSubSystem,
+    SDL_PauseAudio,
+    SDL_INIT_AUDIO
+};
+use std::os::raw::c_void;
 
 mod mixer;
 mod ffi;
+
+pub use mixer::Mixer;
 
 pub const PX_SOUND_STOP: u32 = 0;
 pub const PX_SOUND_LOOP: u32 = 1;
@@ -42,4 +54,39 @@ impl SoundSource {
         let ptr = &self.samples as *const u16;
         unsafe { slice::from_raw_parts(ptr.offset(1), self.len as usize) }
     }
+}
+
+fn next_round (mut arg: u32) -> u32 {
+    arg -= 1;
+    arg |= arg >> 1;
+    arg |= arg >> 2;
+    arg |= arg >> 4;
+    arg |= arg >> 8;
+    arg |= arg >> 16;
+    arg + 1
+}
+
+pub fn open<'a> (channels: u8, samplerate: i32, bufsecs: f32, format: u16,
+    sounds: &[Sound<'a>]) -> Box<Mixer<'a>>
+{
+    let want = SDL_AudioSpec {
+        freq: samplerate,
+        format,
+        channels,
+        samples: next_round((bufsecs * samplerate as f32) as u32) as u16,
+        callback: Some(Mixer::mix),
+        userdata: unsafe { Box::into_raw(Box::new(Mixer::new(sounds))) as *mut c_void },
+        size: 0,
+        silence: 0,
+        padding: 0,
+    };
+/*
+    if (SDL_InitSubSystem(SDL_INIT_AUDIO) < 0) puts(SDL_GetError());
+    if (!SDL_OpenAudio(&want, &px_audio_spec)) SDL_PauseAudio(0);
+    else printf("Audio failed to open: %s\n", SDL_GetError());
+*/
+    let spec: SDL_AudioSpec = mem::uninitialized();
+    let mixer = Box::from_raw(spec.userdata as *mut Mixer<'a>);
+    mixer.spec = spec;
+    mixer
 }
--- pxaudio/src/mixer.rs
+++ pxaudio/src/mixer.rs
@@ -2,3 +2,46 @@
  * Copyright (C) 2017-2018 Brent Bessemer.
  * All rights reserved.
  */
+
+use super::*;
+
+pub struct Mixer<'a> {
+    pub sounds: &'a [Sound<'a>],
+    pub spec: SDL_AudioSpec,
+}
+
+impl<'a> Mixer<'a> {
+    pub fn new (sounds: &'a [Sound<'a>]) -> Self {
+        Self {
+            sounds,
+            spec: unsafe { mem::uninitialized() },
+        }
+    }
+
+    pub fn mix_typed<T> (&mut self, buffer: &mut [T]) {
+
+    }
+
+    pub fn mix (&mut self, buffer: &mut [u8]) {
+        unsafe { match self.spec.format {
+            AUDIO_U8 => self.mix_typed(buffer),
+            AUDIO_S8 => self.mix_typed(slice::from_raw_parts_mut(
+                buffer.as_mut_ptr() as *mut i8, buffer.len())),
+            AUDIO_U16 => self.mix_typed(slice::from_raw_parts_mut(
+                buffer.as_mut_ptr() as *mut u16, buffer.len() / 2)),
+            AUDIO_S16 => self.mix_typed(slice::from_raw_parts_mut(
+                buffer.as_mut_ptr() as *mut i16, buffer.len() / 2)),
+            AUDIO_S32 => self.mix_typed(slice::from_raw_parts_mut(
+                buffer.as_mut_ptr() as *mut i32, buffer.len() / 4)),
+            AUDIO_F32 => self.mix_typed(slice::from_raw_parts_mut(
+                buffer.as_mut_ptr() as *mut f32, buffer.len() / 4)),
+            _ => ()
+        }}
+    }
+}
+
+#[no_mangle]
+unsafe extern "C" fn pxMixAudio<'a> (mixer: *mut c_void, buffer: *mut u8, buflen: i32) {
+    Mixer::mix(&mut *(mixer as *mut Mixer<'a>),
+        slice::from_raw_parts_mut(buffer, buflen as usize))
+}
