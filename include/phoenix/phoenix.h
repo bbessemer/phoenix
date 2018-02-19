689a1e1b96b055d77193d5645f4bb6e974bba066
Brent Bessemer <bmb0610@gmail.com>
2018-02-19 12:59:14 -0500

    Added C++ header guards

--- include/phoenix/phoenix.h
+++ include/phoenix/phoenix.h
@@ -1,5 +1,9 @@
 #pragma once
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include "box.h"
 #include "color.h"
 #include "error.h"
@@ -9,5 +13,11 @@
 #include "timer.h"
 #include "font.h"
 #include "audio.h"
+#include "imgfile.h"
+#include "ttfont.h"
+
+#ifdef __cplusplus
+}
+#endif
 
 //#define SDL_main main
